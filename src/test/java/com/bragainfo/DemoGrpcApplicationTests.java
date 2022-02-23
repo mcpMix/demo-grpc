8f3d466ceb351460fce736056bf1bf68d19da7ee
CRISTIANO B FURTADO <cristiano.furtado.ext@e-ai.com.br>
2022-02-23 17:34:41 -0300

    first commit

---
+++ src/test/java/com/bragainfo/DemoGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bragainfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGrpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

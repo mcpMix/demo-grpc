ea3e9e8f7b58e1acf126c3b07617a525a6292b10
CRISTIANO B FURTADO <cristiano.furtado.ext@e-ai.com.br>
2022-02-24 12:44:41 -0300

    Finished hello world

--- src/main/java/com/bragainfo/controller/HelloController.java
+++ src/main/java/com/bragainfo/controller/HelloController.java
@@ -1,4 +1,23 @@
 package com.bragainfo.controller;
 
-public class HelloController {
+
+import com.bragainfo.HelloReq;
+import com.bragainfo.HelloRes;
+import com.bragainfo.HelloServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class HelloController extends HelloServiceGrpc.HelloServiceImplBase {
+
+    @Override
+    public void hello(HelloReq request, StreamObserver<HelloRes> responseObserver) {
+            var response = HelloRes.newBuilder()
+                    .setMessage(request.getMessage())
+                    .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+
+    }
 }
